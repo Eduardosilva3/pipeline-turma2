fd2afca696d856e25e8f55e5cab309b678af8c4c
Eduardosilva3 <eduardo-silva159@hotmail.com>
2022-08-31 20:40:24 -0300

    Subindo projeto java

---
+++ src/main/java/com/teste/RodandaoPipeline/RodandaoPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.teste.RodandaoPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodandaoPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodandaoPipelineApplication.class, args);
+	}
+
+}
